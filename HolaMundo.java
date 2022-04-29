884fda6c3713cdb1dd6f7ee5e9de5334cc3d3564
ValentinaCham <90291981+ValentinaCham@users.noreply.github.com>
2022-04-29 09:47:32 -0500

    Agregado de edad en Información Personal

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,6 +6,7 @@
           String apellido = "Chambilla";
           System.out.println ("¡Hola mundo!");
           System.out.println ("Mi nombre es " + name + apellido);
-          
+          int edad = 19;
+          System.out.println("Tengo "+edad);
       }
   }
