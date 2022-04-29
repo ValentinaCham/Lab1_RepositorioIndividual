f557c43e2865790f80a53b52fcca1356bc943890
ValentinaCham <90291981+ValentinaCham@users.noreply.github.com>
2022-04-29 09:46:01 -0500

    Agregado de Nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,10 @@
   {
       public static void main(String args[]) {
       
-          /** */
+          String name = "Valentina";
+          String apellido = "Chambilla";
           System.out.println ("¡Hola mundo!");
-
+          System.out.println ("Mi nombre es " + name + apellido);
+          
       }
   }
